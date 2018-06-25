d8b035be0cd066c3a2fac758e0bdad4aeea007b1
Klaas <klaashermans@gmail.com>
2018-06-25 08:22:24 +0200

    WLC added

--- Tools.py
+++ Tools.py
@@ -31,11 +31,11 @@ def get_rotation_data(data, headers):
     Z = data[:,headers.index('Z0'+' (um)')::4]
     X = data[:,headers.index('X0'+' (um)')::4]
     Y = data[:,headers.index('Y0'+' (um)')::4]
-    T=T[Rot != 0]    
-    X=X[Rot != 0]
-    Y=Y[Rot != 0]
-    Z=Z[Rot != 0]
-    Rot=Rot[Rot != 0]
+#    T=T[Rot != 0]    
+#    X=X[Rot != 0]
+#    Y=Y[Rot != 0]
+#    Z=Z[Rot != 0]
+#    Rot=Rot[Rot != 0]
     return X,Y,Z, Rot, T
 
 def get_force_data(data, headers):
@@ -65,12 +65,44 @@ def default_pars():
     """Default fitting parameters, returns a {dict} with 'key'= paramvalue"""
     par = {}
     par['L_bp']= 4139
+    par['P_nm'] = 50
+    par['S_pN'] = 1200
+    par['dsDNA_nm_bp']=0.34
+    par['z0_nm'] = 0
     par['Pitch_nm'] = 10.4
-    par['MeltingTorque_pN'] = 0.16
+    par['MeltingTorque_pN_nm'] = 0.16
     par['Torque'] = 0
     par['kBT_pN_nm'] = 4.2 #pn/nm 
     par['MeasurementERR (nm)'] = 5     #tracking inaccuracy in nm
     return par
+
+def wlc(force,Pars): #in nm/pN, as fraction of L
+    """Calculates WLC in nm/pN.
+    Returns Z_WLC in nm"""
+    f = np.array(force)
+    return Pars['L_bp']*Pars['dsDNA_nm_bp']*(1 - 0.5*(np.sqrt(Pars['kBT_pN_nm']/(f*Pars['P_nm'])))+(f/Pars['S_pN'])) + Pars['z0_nm']
+
+def wlc_fit(f, P, S, Pars):
+    return Pars['L_bp']*Pars['dsDNA_nm_bp']*(1 - 0.5*(np.sqrt(Pars['kBT_pN_nm']/(f*P))+(f/S))) + Pars['z0_nm']
+
+def offset_fit(f, z0, Pars):
+    return Pars['L_bp']*Pars['dsDNA_nm_bp']*(1 - 0.5*(np.sqrt(Pars['kBT_pN_nm']/(f*Pars['P_nm'])))+(f/Pars['S_pN'])) + z0
+
+def motor_pos(LogFile, MotorName):
+    """Open the corresponding .log files from magnetic tweezers. Returns False if the file is not found"""
+    try: 
+        f = open(LogFile, 'r')
+    except FileNotFoundError: 
+        print(LogFile, '========> No valid logfile found')
+        return 0   
+    content = f.readlines()
+    f.close()
+    for lines in content:
+        P =lines.split(' = ')
+        if P[0]==MotorName:
+            return round(float(P[1].strip('\n')),1)
+    print("<<<<<<<<<<", MotorName, "not found >>>>>>>>>>")
+    return 0
 
 def plot_sigma(Sigma,Z,fig1):
     ax1 = fig1.add_subplot(1, 2, 1)
@@ -80,8 +112,8 @@ def plot_sigma(Sigma,Z,fig1):
     ax1.scatter(Sigma, Z, color='black', lw=0.1, s=5)
 
 def plot_force(F,Z,fig1):
-    ax1 = fig1.add_subplot(1, 2, 1)
-    ax1.set_title('Force Extension')
-    ax1.set_ylabel(r'Extension')
-    ax1.set_xlabel(r'Force')
-    ax1.scatter(F, Z, color='black', lw=0.1, s=5)
+    ax2 = fig1.add_subplot(1, 2, 2)
+    ax2.set_title('Force Extension')
+    ax2.set_ylabel(r'Extension')
+    ax2.set_xlabel(r'Force')
+    ax2.scatter(F, Z, color='black', lw=0.1, s=5)
